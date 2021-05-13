225da2a6374c5dabb0f3e294050f881458a2d528
EugeneJoe <jmuthui70@gmail.com>
2021-05-13 20:18:56 +0000

    Remove unnecessary checks

--- rotl.c
+++ rotl.c
@@ -11,8 +11,6 @@
  */
 void rotl_stack(stack_t **head, unsigned int line_number)
 {
-	if ((*head) == NULL || head == NULL)
-		return;
 	int_arg = (*head)->n;
 	pop_stack(head, line_number);
 	push_queue(head, line_number);
